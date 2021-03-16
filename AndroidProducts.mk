c0d1ae324890d0b625569e1d2ac29b7df9460237
Al Sutton <al.sutton@alsutton.com>
2021-03-16 16:46:08 +0000

    Initial import

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+# Copyright (c)2021 Al Sutton
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/sa_car_x86.mk
+
+COMMON_LUNCH_CHOICES := sa_car_x86-userdebug
+
+EMULATOR_VENDOR_NO_SOUND_TRIGGER := false
---
+++ sa_car_x86.mk
@@ -0,0 +1,12 @@
+# Copyright (c)2021 Al Sutton
+
+PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
+
+$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
+
+EMULATOR_VENDOR_NO_SOUND := true
+PRODUCT_NAME := sa_car_x86
+PRODUCT_DEVICE := generic_x86
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Snapp Automotive on x86 emulator
