0ae6642c41d55ceec9b59fe856406f3de26def54
Al Sutton <al.sutton@alsutton.com>
2021-04-27 17:47:46 +0100

    Update to use common device overrides

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -1,18 +1,13 @@
-# Copyright (c)2021 Al Sutton
-
-PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay device/snappautomotive/emulator/overlay
+PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
 
 PRODUCT_COPY_FILES += \
-    device/snappautomotive/emulator/bootanimations/bootanimation.zip:system/media/bootanimation.zip \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
 
-
+include device/snappautomotive/common/additions.mk
 
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
-
-PRODUCT_PACKAGES += osmdroid
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
