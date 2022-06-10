53c8587dc95b2833f2beb1ca16ca5ed91634a32f
Al Sutton <al.sutton@alsutton.com>
2022-06-10 09:21:11 +0100

    Makefile cleanup
    Re-order the includes to follow the AOSP standard for least to most
    specific ordering
    
    Update the product to use a more generic value so AS recognises it
    
    Remove redundant lines

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -15,6 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
-PRODUCT_DEVICE := generic_car_x86
+PRODUCT_DEVICE := generic_x86
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Snapp Automotive on x86 emulator
+PRODUCT_MODEL := Snapp Automotive AAOS Image for x86 emulator
