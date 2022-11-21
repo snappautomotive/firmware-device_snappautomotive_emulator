01fb4d8e27753c06ec3b7a82de4cd436c01d185d
Al Sutton <al.sutton@alsutton.com>
2022-11-21 10:04:27 +0000

    Add 64 bit build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,10 +2,12 @@
 
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/snapp_car_x86.mk \
-        $(LOCAL_DIR)/snapp_car_arm64.mk
+	$(LOCAL_DIR)/snapp_car_x86_64.mk \
+        $(LOCAL_DIR)/snapp_car_arm64.mk 
 
 COMMON_LUNCH_CHOICES := \
 	snapp_car_x86-userdebug \
+	snapp_car_x86_64-userdebug \
 	snapp_car_arm64-userdebug
 
 EMULATOR_VENDOR_NO_SOUND_TRIGGER := false
