1647c6bcdda04af14f13eb1fa6cd111ffb6fde88
Al Sutton <al.sutton@alsutton.com>
2022-11-21 10:04:13 +0000

    Use the SDK base to be consistent with other emulator builds

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -11,7 +11,7 @@ EXCLUDE_FIRMWARE_UPDATER := true
 include device/snappautomotive/common/additions.mk
 
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
