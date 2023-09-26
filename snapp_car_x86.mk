868c57c7a53c8e1c4407e47b4e7cda60a9451444
Al Sutton <al.sutton@alsutton.com>
2023-09-26 17:49:31 +0100

    Add car audio makefile

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -12,6 +12,7 @@ include device/snappautomotive/common/additions.mk
 
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86.mk)
+$(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
