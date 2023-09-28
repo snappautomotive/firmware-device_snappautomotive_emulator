d3d167fe0884fdfd42aef046e11c66cb335d6317
Al Sutton <al.sutton@alsutton.com>
2023-09-28 10:55:03 +0100

    Simplify makefiles

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -1,15 +1,11 @@
-PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
-
-EMULATOR_VENDOR_NO_SENSORS := true
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+PRODUCT_PACKAGE_OVERLAYS := \
+    device/generic/car/common/overlay \
+    device/snappautomotive/emulator/overlay
 
 EXCLUDE_FIRMWARE_UPDATER := true
+$(call inherit-product, device/snappautomotive/common/additions.mk)
 
-include device/snappautomotive/common/additions.mk
-
+EMULATOR_VENDOR_NO_SENSORS := true
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 $(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
