56ef5a71b175231a558f30198347efd168efff9d
Al Sutton <al.sutton@alsutton.com>
2023-09-28 10:55:18 +0100

    Remove x86 build because it currently fails to completely boot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 # Copyright (c)2021 Al Sutton
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/snapp_car_x86.mk \
 	$(LOCAL_DIR)/snapp_car_x86_64.mk \
         $(LOCAL_DIR)/snapp_car_arm64.mk 
 
+#	$(LOCAL_DIR)/snapp_car_x86.mk \ <-- Disabled, does not boot
+
 COMMON_LUNCH_CHOICES := \
-	snapp_car_x86-userdebug \
 	snapp_car_x86_64-userdebug \
 	snapp_car_arm64-userdebug
 
