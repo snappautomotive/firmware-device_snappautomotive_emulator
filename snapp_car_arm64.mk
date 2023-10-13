52625a554e50c708994831a4aff3bcdb90c9b9d9
Al Sutton <al.sutton@alsutton.com>
2023-10-13 07:28:57 +0100

    Use as much of the existing AOSP makefiles as possible

--- snapp_car_arm64.mk
+++ snapp_car_arm64.mk
@@ -2,13 +2,8 @@ PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
 
 EXCLUDE_FIRMWARE_UPDATER := true
 
-
 $(call inherit-product, device/snappautomotive/common/additions.mk)
-$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_arm64.mk)
+$(call inherit-product, device/generic/car/aosp_car_arm64.mk)
 
-EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_arm64
-PRODUCT_DEVICE := emulator_arm64
-PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for ARM64
