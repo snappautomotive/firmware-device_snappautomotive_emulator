4d6ea2b4704bef4cd3f4b05cad6cf6e0c1d22aba
Al Sutton <al.sutton@alsutton.com>
2023-10-19 09:23:57 +0100

    Work around broken generic_x86_64 makefile
    Change I43c8005afcaeec6ed70648b5f74eeed1d46f9228 altered
    generic_x86_64/device.mk so that it fails so build images which can be
    used in an emulator, so we need to switch to another board type to
    ensure the target builds an emulator image that can be booted.
    
    Change Ref: https://android-review.googlesource.com/c/platform/build/+/1699575

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -1,10 +1,22 @@
-PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
+#
+# x86_64 definition. We can't rely on device/generic/car/aosp_car_x86_64.mk
+# because it defines the PRODUCT_DEVICE as generic_car_x86_64, and the product
+# makefile for that is not compatible with the `emu_img_zip` make target
+#
+
+PRODUCT_PACKAGE_OVERLAYS := \
+	device/generic/car/common/overlay \
+	device/snappautomotive/emulator/overlay
 
 EXCLUDE_FIRMWARE_UPDATER := true
-
 $(call inherit-product, device/snappautomotive/common/additions.mk)
-$(call inherit-product, device/generic/car/aosp_car_x86_64.mk)
 
+EMULATOR_VENDOR_NO_SENSORS := true
+$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_phone_x86_64.mk)
+
+EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
 PRODUCT_DEVICE := emulator_x86_64
+PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for x86_64
