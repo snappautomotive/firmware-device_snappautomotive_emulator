04fb70fae5fb6c4cc7fb66c243f159628bf681aa
Al Sutton <al.sutton@alsutton.com>
2023-12-20 13:26:21 +0000

    Add missing service

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -2,10 +2,12 @@ PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
 
 EXCLUDE_FIRMWARE_UPDATER := true
 
-
 $(call inherit-product, device/snappautomotive/common/additions.mk)
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.3-impl
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
