ce3d371479a5f9d686c6efe1d862dbad2116b17c
Al Sutton <al.sutton@alsutton.com>
2023-12-21 13:50:37 +0000

    Add EVS service

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -4,13 +4,13 @@ EXCLUDE_FIRMWARE_UPDATER := true
 ENABLE_REAR_VIEW_CAMERA_SAMPLE := true
 ENABLE_EVS_SAMPLE := true
 
-
 $(call inherit-product, device/snappautomotive/common/additions.mk)
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.automotive.evs@1.1-service
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
