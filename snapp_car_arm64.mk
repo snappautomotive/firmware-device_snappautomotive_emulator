91aa89f42a0be9033ea37480b5819044b0b4cac3
coding-chick <efratbarak@gmail.com>
2024-04-05 13:22:32 +0200

    updated build files

--- snapp_car_arm64.mk
+++ snapp_car_arm64.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
 EXCLUDE_FIRMWARE_UPDATER := true
 $(call inherit-product, device/snappautomotive/common/additions.mk)
 
-$(call inherit-product, device/generic/car/sdk_car_arm64.mk)
+$(call inherit-product, device/snappautomotive/emulator/sdk_car_arm64_fixed.mk)
+
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 PRODUCT_NAME := snapp_car_arm64
 PRODUCT_MODEL := Snapp Automotive on arm64 emulator
--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -5,5 +5,7 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 
 $(call inherit-product, device/generic/car/sdk_car_x86_64.mk)
 
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 PRODUCT_NAME := snapp_car_x86_64
 PRODUCT_MODEL := Snapp Automotive emulator for x86_64
