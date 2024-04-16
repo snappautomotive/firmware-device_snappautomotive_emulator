c37d1f4a5451faba1871f9fb5078c28dbca4cf7c
Al Sutton <al.sutton@alsutton.com>
2024-04-16 10:41:13 +0100

    Update PRODUCT_DEVICE for consistency

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -6,4 +6,5 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 $(call inherit-product, device/generic/car/sdk_car_x86.mk)
 
 PRODUCT_NAME := snapp_car_x86
+PRODUCT_DEVICE := emulator_car_x86
 PRODUCT_MODEL := Snapp Automotive on x86 emulator
