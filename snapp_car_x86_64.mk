a02352557a7b503684dad955f40473187777fbba
Al Sutton <al.sutton@alsutton.com>
2024-05-10 16:40:46 +0100

    Go back to the SDK target so we can get the emulator ramdisk included in the build

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -5,7 +5,7 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 
 EMULATOR_VENDOR_NO_SENSORS := true
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
