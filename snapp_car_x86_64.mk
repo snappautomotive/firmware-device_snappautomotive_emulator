5f4388d0177db971832f6ad7267ad4084a52a625
Al Sutton <al.sutton@alsutton.com>
2024-05-10 15:43:35 +0100

    Synchronize with AOSP build file

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -5,11 +5,10 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 
 EMULATOR_VENDOR_NO_SENSORS := true
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
-$(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
-PRODUCT_DEVICE := emulator_x86_64
+PRODUCT_DEVICE := generic_car_x86_64
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for x86_64
