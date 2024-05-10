c0084618aed81ff2dabf0a913ba3dd07de072c05
Al Sutton <al.sutton@alsutton.com>
2024-05-10 13:35:49 +0100

    Simplify makefile to see if we can identify the cause of the boot issue

--- snapp_car_x86_64.mk
+++ snapp_car_x86_64.mk
@@ -8,23 +8,8 @@ $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 $(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
 
-PRODUCT_SDK_ADDON_COPY_FILES := \
-    device/generic/goldfish/data/etc/encryptionkey.img:images/${TARGET_CPU_ABI}/encryptionkey.img \
-    device/generic/car/car_x86_64/manifest.ini:manifest.ini
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
-
-DEVICE_PACKAGE_OVERLAYS += device/generic/car/car_x86_64/overlay
-
-PRODUCT_PACKAGES += android.hardware.automotive.vehicle@2.0-default-service
-
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86_64
 PRODUCT_DEVICE := emulator_x86_64
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for x86_64
-
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := \
-    device/generic/car/car_x86_64/source.properties
