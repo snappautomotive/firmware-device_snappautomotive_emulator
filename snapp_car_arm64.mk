5adb6f720fbcecaff9681c52be1aa7aa846e392c
Al Sutton <al.sutton@alsutton.com>
2024-07-22 16:46:32 +0100

    Unify build targets to use the emulator device type

--- snapp_car_arm64.mk
+++ snapp_car_arm64.mk
@@ -8,7 +8,6 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 EMULATOR_VENDOR_NO_SENSORS := true
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_arm64.mk)
-$(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_arm64
--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -8,13 +8,9 @@ $(call inherit-product, device/snappautomotive/common/additions.mk)
 EMULATOR_VENDOR_NO_SENSORS := true
 $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86.mk)
-$(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
-
-# Packages needed to support x86 CarService startup
-PRODUCT_PACKAGES += android.hardware.automotive.vehicle@2.0-default-service
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
-PRODUCT_DEVICE := generic_car_x86
+PRODUCT_DEVICE := emulator_x86
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for x86
