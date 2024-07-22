087a0c3b82b5136bee0262726e143c1d61beb1b4
Al Sutton <al.sutton@alsutton.com>
2024-07-22 13:28:31 +0100

    Use the emulator device target for all three platforms

--- snapp_car_x86.mk
+++ snapp_car_x86.mk
@@ -9,6 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86.mk)
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := snapp_car_x86
-PRODUCT_DEVICE := generic_car_x86
+PRODUCT_DEVICE := emulator_x86
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Snapp Automotive emulator for x86
