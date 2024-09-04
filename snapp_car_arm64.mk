6a38b8d59c6fb068f5e8cddefc80a3f295f80e37
Al Sutton <al.sutton@alsutton.com>
2024-09-04 08:25:22 +0100

    Android 15 fixed the issue with the ARM64 target

--- snapp_car_arm64.mk
+++ snapp_car_arm64.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGE_OVERLAYS := device/snappautomotive/emulator/overlay
 EXCLUDE_FIRMWARE_UPDATER := true
 $(call inherit-product, device/snappautomotive/common/additions.mk)
 
-$(call inherit-product, device/snappautomotive/emulator/sdk_car_arm64_fixed.mk)
+$(call inherit-product, device/generic/car/sdk_car_arm64.mk)
 
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
